00babb7a985d01ac86025fbf45be219953d92ca8
Will Temple <will@wtemple.net>
2023-12-12 12:31:11 -0500

    Some changes.
    - Generated derive macro calls using decl macro for DRY.
    - Ran clippy & fixed warnings.
    - Removed spurious `Output` assoc. types from From impls.
    - Eliminated a few indirections using destructuring.
    - Slightly changed formatting of outputs.
    - Removed most unnecessary uses of `return`.
    - Changed how proc_macro::TokenStream and proc_macro2::TokenStream are
      used.

--- src/lib.rs
+++ src/lib.rs
@@ -1,154 +1,127 @@
-use proc_macro::TokenStream;
-use proc_macro2::Span;
+use proc_macro::TokenStream as CompilerTokenStream;
+use proc_macro2::{Span, TokenStream};
 
 use quote::quote;
-use syn::{
-    parse,
-    token::{AndEq, CaretEq, MinusEq, OrEq, PercentEq, PlusEq, ShlEq, ShrEq, SlashEq, StarEq},
-    BinOp, DeriveInput, Ident,
-};
+use syn::{parse, BinOp, DeriveInput, Ident, Token};
 
-/*
- * This macro generates a trait that implements an operator from an operator assignment
- * and the "Clone" trait.
- */
+//
+// This macro generates a trait that implements an operator from an operator assignment
+// and the "Clone" trait.
+//
 
-fn op_from_op_assign(input: TokenStream, op_name: &str, operator: BinOp) -> TokenStream {
-    let ast: DeriveInput = parse(input).unwrap();
-    let name = &ast.ident;
-    let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
-    let trait_name = Ident::new(&format!("{}", op_name), Span::call_site());
-    let function_name = Ident::new(&format!("{}", op_name.to_lowercase()), Span::call_site());
-    let gen = quote! {
+fn op_from_op_assign(input: CompilerTokenStream, op_name: &str, operator: BinOp) -> TokenStream {
+    let DeriveInput {
+        ident: name,
+        generics,
+        ..
+    } = parse(input).unwrap();
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let trait_name = Ident::new(op_name, Span::call_site());
+    let function_name = Ident::new(&op_name.to_lowercase(), Span::call_site());
+
+    quote! {
         impl #impl_generics #trait_name for #name #ty_generics #where_clause {
             type Output = Self;
+
             fn #function_name (self, rhs: Self) -> Self {
                 let mut ret = self.clone();
+
                 ret #operator rhs;
-                return ret;
+
+                ret
+            }
+        }
+    }
+}
+
+macro_rules! derive_for_assign_op {
+    ($name:ident for $t:tt) => {
+        paste::paste! {
+            #[proc_macro_derive($name)]
+            pub fn [<$name:lower _for_ $name:lower _assign>](input: CompilerTokenStream) -> CompilerTokenStream {
+                op_from_op_assign(input, stringify!($name), syn::BinOp::[<$name Assign>](<Token![$t]>::default())).into()
             }
         }
     };
-    gen.into()
 }
 
-#[proc_macro_derive(Add)]
-pub fn add_from_add_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Add", syn::BinOp::AddAssign(PlusEq::default()))
-}
+derive_for_assign_op!(Add for +=);
+derive_for_assign_op!(Sub for -=);
+derive_for_assign_op!(Mul for *=);
+derive_for_assign_op!(Rem for %=);
+derive_for_assign_op!(BitXor for ^=);
+derive_for_assign_op!(BitAnd for &=);
+derive_for_assign_op!(BitOr for |=);
+derive_for_assign_op!(Shl for <<=);
+derive_for_assign_op!(Shr for >>=);
 
-#[proc_macro_derive(Sub)]
-pub fn sub_from_sub_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Sub", syn::BinOp::SubAssign(MinusEq::default()))
-}
-
-#[proc_macro_derive(Mul)]
-pub fn mul_from_mul_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Mul", syn::BinOp::MulAssign(StarEq::default()))
-}
-
-#[proc_macro_derive(Div)]
-pub fn div_from_div_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Div", syn::BinOp::DivAssign(SlashEq::default()))
-}
-
-#[proc_macro_derive(Rem)]
-pub fn rem_from_rem_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Rem", syn::BinOp::RemAssign(PercentEq::default()))
-}
-
-#[proc_macro_derive(BitXor)]
-pub fn bitxor_from_bitxor_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(
-        input,
-        "BitXor",
-        syn::BinOp::BitXorAssign(CaretEq::default()),
-    )
-}
-
-#[proc_macro_derive(BitAnd)]
-pub fn bitand_from_bitand_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "BitAnd", syn::BinOp::BitAndAssign(AndEq::default()))
-}
-
-#[proc_macro_derive(BitOr)]
-pub fn bitor_from_bitor_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "BitOr", syn::BinOp::BitOrAssign(OrEq::default()))
-}
-
-#[proc_macro_derive(Shl)]
-pub fn shl_from_shl_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Shl", syn::BinOp::ShlAssign(ShlEq::default()))
-}
-
-#[proc_macro_derive(Shr)]
-pub fn shr_from_shr_assign(input: TokenStream) -> TokenStream {
-    op_from_op_assign(input, "Shr", syn::BinOp::ShrAssign(ShrEq::default()))
-}
-
-/*
- * These macros generate a "From" trait for integral types, assuming the type has a "From" trait
- * defined for a 64-bit integral type.
- */
+//
+// These macros generate a "From" trait for integral types, assuming the type has a "From" trait
+// defined for a 64-bit integral type.
+//
 
 #[proc_macro_derive(FromUnsigned)]
-pub fn from_unsigned(input: TokenStream) -> TokenStream {
-    let ast: DeriveInput = parse(input).unwrap();
-    let name = &ast.ident;
-    let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
-    let gen = quote! {
+pub fn from_unsigned(input: CompilerTokenStream) -> CompilerTokenStream {
+    let DeriveInput {
+        ident: name,
+        generics,
+        ..
+    } = parse(input).unwrap();
 
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    quote! {
         impl #impl_generics From<u8> for #name #ty_generics #where_clause {
-            type Output = Self;
             fn from(input: u8) -> Self {
-                return Self::from(input as u64);
+                Self::from(input as u64)
             }
         }
 
         impl #impl_generics From<u16> for #name #ty_generics #where_clause {
-            type Output = Self;
             fn from(input: u16) -> Self {
-                return Self::from(input as u64);
+                Self::from(input as u64)
             }
         }
 
         impl #impl_generics From<u32> for #name #ty_generics #where_clause {
-            type Output = Self;
             fn from(input: u32) -> Self {
-                return Self::from(input as u64);
+                Self::from(input as u64)
             }
         }
-    };
-    gen.into()
+    }
+    .into()
 }
 
 #[proc_macro_derive(FromSigned)]
-pub fn from_signed(input: TokenStream) -> TokenStream {
-    let ast: DeriveInput = parse(input).unwrap();
-    let name = &ast.ident;
-    let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
-    let gen = quote! {
+pub fn from_signed(input: CompilerTokenStream) -> CompilerTokenStream {
+    let DeriveInput {
+        ident: name,
+        generics,
+        ..
+    } = parse(input).unwrap();
 
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    quote! {
         impl #impl_generics From<i8> for #name #ty_generics #where_clause {
-            type Output = Self;
             fn from(input: i8) -> Self {
-                return Self::from(input as i64);
+                Self::from(input as i64)
             }
         }
 
         impl #impl_generics From<i16> for #name #ty_generics #where_clause {
-            type Output = Self;
             fn from(input: i16) -> Self {
-                return Self::from(input as i64);
+                Self::from(input as i64)
             }
         }
 
         impl #impl_generics From<i32> for #name #ty_generics #where_clause {
-            type Output = Self;
             fn from(input: i32) -> Self {
-                return Self::from(input as i64);
+                Self::from(input as i64)
             }
         }
-    };
-    gen.into()
+    }
+    .into()
 }
